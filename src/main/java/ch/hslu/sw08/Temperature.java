626a7f251d45ad5d1858d71ebf599f5c876ea9ef
dave1b <57140925+dave1b@users.noreply.github.com>
2020-11-23 20:13:34 +0100

    Small updateds.

--- src/main/java/ch/hslu/sw08/Temperature.java
+++ src/main/java/ch/hslu/sw08/Temperature.java
@@ -1,149 +1,146 @@
 package ch.hslu.sw08;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
-* Eine Klasse mit der man Temperaturen in verschiedenen Einheiten ausgeben kann 
-* und den Aggregatszustand von 3 Elementen ausgeben kann.
-* 
-* @author Dave Brunner
-* @version Version: 1.1 
-*/
-
-
-
-
-
-
+ * Eine Klasse mit der man Temperaturen in verschiedenen Einheiten ausgeben kann 
+ * und den Aggregatszustand von 3 Elementen ausgeben kann.
+ * 
+ * @author Dave Brunner
+ * @version Version: 1.1 
+ */
 
 public class Temperature implements Comparable<Temperature>
 {
 	private static final Logger LOG = LogManager.getLogger(Temperature.class);
-	final static float kelvinOffset = 273.15f;
-	
-	// The temperature is stated in °C
-	protected float temp;
-	
+	final static float KELVINOFFSET = 273.15f;
 
-	
-	
-	
-	
+
+	// The temperature is stated in °C
+	private float temp;
+
+
+	public enum TempType {
+		Celsius(), Kelvin(), Gurke();
+	} 
+
+
+
+
 	/**
-	 * Factory-Methode für Objekte mit einer Celcious-Temperatur.
-	 * @param Grad in Celcious.
+	 * Factory-Methode für Objekte mit einer Celsius-Temperatur.
+	 * @param Grad in Celsius.
 	 * @return ein Temperatur-Objekt.
 	 */
-	final public static Temperature createFromCelcious(final float celcious) {
-		LOG.debug("createFromCelcious: Anfang");
-		if(celcious < -273.15f) {
-			throw new IllegalArgumentException("The entered temperature must be >= -273.15f");
+	final public static Temperature createFromCelsius(final float celsius) {
+		LOG.debug("createFromCelsius: Anfang");
+		if(celsius < -KELVINOFFSET) {
+			throw new IllegalArgumentException("The entered temperature must be >= "+ -KELVINOFFSET);
 		} else {
-			LOG.debug("createFromCelcious: Ende");	
-			return new Temperature("c", celcious);		
+			LOG.debug("createFromCelsius: Ende");	
+			return new Temperature(TempType.Celsius, celsius);		
 		}		
 	}
-	
+
 	/**
 	 * Factory-Methode für Objekte mit einer Kelvin-Temperatur.
 	 * @param Grad in Kelvin
-	 * @return ein Temperatur-Objekt
+	 * @return ein Temperatur-Objekt.
 	 */
 	final public static Temperature createFromKelvin(final float kelvin) {
 		if(kelvin < 0f) {
-			throw new IllegalArgumentException("The entered temperature must be >= 0");
+			throw new IllegalArgumentException("The entered temperature must be >= 0f");
 		} else {
-			return new Temperature("k", kelvin);
+			return new Temperature(TempType.Kelvin, kelvin);
 		}
 	}
 
- 
 
-   /**
-    * Einfacher Konstruktor.
-    * @param Temperatur in Grad Celsious als float.
-    * 
-    */
-   public Temperature(float temp)
-   {
-	   // Instanzvariable initialisieren
-	   this.temp = temp;
-   }
-   /**
-    * Konstruktor mit der Option den Temperaturwert in Celsius oder Grad anzugeben.
-    * @param typ entweder "c" für Celsious oder "k" für Kelvin. Wirft eine IllegalArgumentException bei nichteinhalten der Parametervorgaben.
-    * 
-    */
-   public Temperature(String typ, float temp)
-   {
-	   if(typ.equals("c")){
-		   this.temp = temp;
-	   } else if(typ.equals("k")) {
-		   setTemperaturKelvin(temp);
-	   } else {
-		   throw new IllegalArgumentException("The String typ must either be 'c' or 'k'!");
-	   }
-   }
 
-   /*
-    * Konstruktor mit Default-Initialwert.
-    */
-    public Temperature()
-   {
-       // Instanzvariable initialisieren
-       this.temp = 20f;      
-   }
-   
-    
-    
-    
-    
-    
-    
-    
-   
+	/**
+	 * Einfacher Konstruktor.
+	 * @param Temperatur in Grad Celsious als float.
+	 * 
+	 */
+	public Temperature(float temp)
+	{
+		// Instanzvariable initialisieren
+		this.temp = temp;
+	}
+	/**
+	 * Konstruktor mit der Option den Temperaturwert in Celsius oder Grad anzugeben.
+	 * @param Enum als typ entweder TempType.Celsius für Celsious oder TempType.Kelcin für Kelvin. Wirft eine IllegalArgumentException bei nichteinhalten der Parametervorgaben.
+	 * 
+	 */
+	public Temperature(TempType typ, float temp)
+	{
+		if(typ.equals(TempType.Celsius)){
+			this.temp = temp;
+		} else if(typ.equals(TempType.Kelvin)) {
+			this.temp = convertTemperatureFromKelvinToCelsious(temp);
+		} else {
+			throw new IllegalArgumentException("The Enum typ must either be 'TempType.Celsius' or 'TempType.Kelvin!'");
+		}
+	}
 
-   /**
-    * @return der Temperatur in Grac Celsious als float.
-    */
-   final public float getTemperatureInCelcious()
-   {
-       return temp;
-   }
-   
-   /**
-    * Umwandlung von Kelvin nach Celcious.
-    * @param temp in Kelvin.
-    */
-   final public float getTemperatureInKelvin()
-   {
-	   return  this.temp+kelvinOffset;
-	   
-   }
+	/*
+	 * Konstruktor mit Default-Initialwert.
+	 */
+	public Temperature()
+	{
+		// Instanzvariable initialisieren
+		this.temp = 20f;      
+	}
 
-   /**
-    * Ausgabe der Temperatur in Fahrenheit.
-    */
-   final public float getTemperatureInFahrenheit()
-   {
-	   return (this.temp * 1.8f) + 32f;
-	   
-   }
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
 
-   
-	   
+
+
+
+
+
+
+
+
+	/**
+	 * @return der Temperatur in Grac Celsious als float.
+	 */
+	final public float getTemperatureInCelsius()
+	{
+		return temp;
+	}
+
+	/**
+	 * Umwandlung von Kelvin nach Celsius.
+	 * @param temp in Kelvin.
+	 */
+	final public float getTemperatureInKelvin()
+	{
+		return  this.temp+KELVINOFFSET;
+
+	}
+
+	/**
+	 * Ausgabe der Temperatur in Fahrenheit.
+	 */
+	final public float getTemperatureInFahrenheit()
+	{
+		return (this.temp * 1.8f) + 32f;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	@Override
 	final public int hashCode() {
 		final int prime = 31;
@@ -151,8 +148,8 @@ public class Temperature implements Comparable<Temperature>
 		result = prime * result + Float.floatToIntBits(temp);
 		return result;
 	}
-	
-	
+
+
 	@Override
 	final public boolean equals(Object obj) {
 		if (this == obj)
@@ -171,133 +168,133 @@ public class Temperature implements Comparable<Temperature>
 
 
 	@Override
-	final public String toString() {
+	public String toString() {
 		return "Temperature [temp=" + temp + "°C]";
 	}
-	
+
 	@Override
 	public int compareTo(Temperature other) {
 		if(this == other) {
 			return 0;
 		} else {	
-		return Float.compare(this.temp, other.temp);
+			return Float.compare(this.getTemperatureInCelsius(), other.getTemperatureInCelsius());
 		}		
 	}
 
 
 
-	
-	
-	
-	
+
+
+
+
 
 	// ------------------------------- Ab hier: Getter & Setter ------------------------------------	   
-	   /**
-	    * Setzen der Temperatur in Celcius.
-	    * @param neuer float-Wert in Celcius.
-	    */
-	   final public void setTemperatureCelcious(float temp)
-	   {
-		   this.temp = temp;
-	   }
-	   
-	   /**
-	    * Relative Veränderung der Temperatur in Celcius.
-	    */
-	   final public void setTemperaturRelativeCelcious(float tempChange)
-	   {
-		   this.temp += tempChange;    
-	   }
+	/**
+	 * Setzen der Temperatur in Celcius.
+	 * @param neuer float-Wert in Celcius.
+	 */
+	final public void setTemperatureCelsius(float temp)
+	{
+		this.temp = temp;
+	}
 
-	   
-	   
-	   
-	  
-	   
-	   /**
-	    * Setzen der Temperatur in Kelvin.
-	    * @param temp in Kelvin
-	    */
-	   final public void setTemperaturKelvin(float temp)
-	   {
-		   this.temp =  temp-kelvinOffset;
-		   
-	   }
-	   
-	   /**
-	    * Relative Veränderung der Temperatur in Kelvin.
-	    */
-	   final public void setTemperaturRelativeKelvin(float tempChange)
-	   {
-	       tempChange +=  -kelvinOffset;
-	       this.temp += tempChange;
-	       
-	   }
-	   
-	   
-	   
-	   
-	   /**
-	    * Relative Veränderung der Temperatur in Fahrenheit.
-	    */
-	   final public void setTemperaturRelativeFahrenheit(float tempChange)
-	   {
-		   tempChange = (tempChange - 32f)/1.8f;
-		   this.temp += tempChange;      
-	   }
-	   
-	   
-	   
-	   
-	   /**
-	    * Umwandlung von Kelvin nach Celcious.
-	    * @param temp in Kelvin.
-	    * @return der Temperatur in Celcius
-	    */
-	   final public float getTemperatureFromKelvinToCelsious(float temp)
-	   {
-		   return  temp-kelvinOffset;
-		   
-	   }
-	   
-	   /**
-	    * Umwandlung von Celcious nach Kelvin.
-	    * @param temp in Celcius.
-	    * @return der Temperatur in Kelvin.
-	    */
-	   final static public float getTemperatureFromCelsiousToKelvin(float temp)
-	   {       
-		   return temp + kelvinOffset;    
-	   }
-   
+	/**
+	 * Relative Veränderung der Temperatur in Celcius.
+	 */
+	final public void setTemperaturRelativeCelsius(float tempChange)
+	{
+		this.temp += tempChange;    
+	}
 
-   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-   
-	   
 
-	   /*
+
+
+
+
+	/**
+	 * Setzen der Temperatur in Kelvin.
+	 * @param temp in Kelvin
+	 */
+	final public void setTemperaturKelvin(float temp)
+	{
+		this.temp =  temp-KELVINOFFSET;
+
+	}
+
+	/**
+	 * Relative Veränderung der Temperatur in Kelvin.
+	 */
+	final public void setTemperaturRelativeKelvin(float tempChange)
+	{
+		tempChange +=  -KELVINOFFSET;
+		this.temp += tempChange;
+
+	}
+
+
+
+
+	/**
+	 * Relative Veränderung der Temperatur in Fahrenheit.
+	 */
+	final public void setTemperaturRelativeFahrenheit(float tempChange)
+	{
+		tempChange = (tempChange - 32f)/1.8f;
+		this.temp += tempChange;      
+	}
+
+
+
+
+	/**
+	 * Umwandlung von Kelvin nach Celsius.
+	 * @param temp in Kelvin.
+	 * @return der Temperatur in Celcius
+	 */
+	final static public float convertTemperatureFromKelvinToCelsious(float temp)
+	{
+		return  temp-KELVINOFFSET;
+
+	}
+
+	/**
+	 * Umwandlung von Celsius nach Kelvin.
+	 * @param temp in Celcius.
+	 * @return der Temperatur in Kelvin.
+	 */
+	final static public float convertTemperatureFromCelsiousToKelvin(float temp)
+	{       
+		return temp + KELVINOFFSET;    
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
 ---------------------- Alte aggregateState-Methode ---------------------------------------------
 
 	public String aggregateState(String element)
@@ -305,7 +302,7 @@ public class Temperature implements Comparable<Temperature>
 		String flüssig = "Flüssig";
 		String fest = "Fest";
 		String gasförmig = "Gasförmig";
-       
+
 		if (element == "N") {   // Wenn nach Stickstoff gefragt wird (N)
 			if( temp > -196f) {
 				return gasförmig;
@@ -315,7 +312,7 @@ public class Temperature implements Comparable<Temperature>
             	return fest;
             }
 		}      
-		
+
        else if (element == "Hg") {  // Wenn nach Quecksilber gefragt wird (Hg)
            if (temp > -38.8344f && temp < 356.73f) {
                return flüssig;
@@ -325,7 +322,7 @@ public class Temperature implements Comparable<Temperature>
                return gasförmig;
            } 
        }
-	
+
        else if (element == "Pb") { // Wenn nach Blei gefragt wird (Pb)
            if (temp < 327.43f) {
                return fest;
@@ -339,12 +336,12 @@ public class Temperature implements Comparable<Temperature>
            System.out.println("Geben Sie ein valides Elementenkürzel ein.");
            System.out.println("'N' , 'Hg', 'Pb' ");
            return "0";
-           
+
        }                 
    }
-   
-   
-	    */
+
+
+	 */
 }
 
 
--- src/main/java/ch/hslu/sw08/TemperatureHistory.java
+++ src/main/java/ch/hslu/sw08/TemperatureHistory.java
@@ -7,53 +7,64 @@ import java.util.List;
 
 
 
+import ch.hslu.sw10.MaxMinListener;
+import ch.hslu.sw10.TemperaturEvent;
+import ch.hslu.sw10.TemperaturEvent.Type;
 
-public class TemperatureHistory extends Temperature implements Comparable<Temperature> {
+public class TemperatureHistory extends Temperature implements Comparable<Temperature>, MaxMinListener {
 
 	final List<Temperature> verlauf = new ArrayList<>();
-	
-	
+	private final List<MaxMinListener> changeListeners = new ArrayList<>();
+
+
 	public void add(float temp) {
-		
+
 		if(!verlauf.contains(new Temperature(temp)))
 		{
-		verlauf.add(new Temperature(temp));
+			if(Float.compare(temp, this.getMaxItem().getTemperatureInCelsius()) == 1 && temp > -Temperature.KELVINOFFSET) {
+				newMax();
+			} else if(Float.compare(temp, this.getMinItem().getTemperatureInCelsius()) == -1 && temp > -Temperature.KELVINOFFSET) {
+				newMin();
+			}
+			verlauf.add(Temperature.createFromCelsius(temp));
 		}
 	}
-	
-	
+
+
 	public void clear() {
 		verlauf.clear();
 	}
-	
+
 	public int getCount() {
 		return verlauf.size();
 	}
+	public Temperature getWithIndex(int index) {
+		return verlauf.get(index);
+	}
 
-	
 	@Override
 	public int compareTo(Temperature other) {
 		if(this == other) {
 			return 0;
 		} else {	
-		return Float.compare(this.temp, other.temp);
+			return Float.compare(this.getTemperatureInCelsius(), other.getTemperatureInCelsius());
 		}	
 	}
-	
-	public float getMaxItem() {
+
+	public Temperature getMaxItem() {
 		if(verlauf.size() > 0) {
-		return (Collections.max(verlauf,null)).getTemperatureInCelcious();
+			return (Collections.max(verlauf));
 		}
-		return 0;
+		return new Temperature();
 	}
-	
-	public float getMinItem() {
+
+	public Temperature getMinItem() {
 		if(verlauf.size() > 0) {
-			return (Collections.min(verlauf,null)).getTemperatureInCelcious();
+			return (Collections.min(verlauf));
 		} 
-			return 0;
+		return new Temperature();			
 	}
-		
+
 	public float getAverage() {
 		int i = 0;
 		float sum = 0f;
@@ -61,15 +72,68 @@ public class TemperatureHistory extends Temperature implements Comparable<Temper
 		while(iterator.hasNext()) {
 			i++;
 			Temperature t = iterator.next();		
-			sum += t.getTemperatureInCelcious();
+			sum += t.getTemperatureInCelsius();
 		}
 		if(i != 0) {
-		return sum/i;
+			return sum/i;
 		} else {
-		return 0f;
+			return 0f;
+		}
+
+
+	}
+
+
+
+
+
+
+
+	public void newMax() {
+		final TemperaturEvent pcEvent = new TemperaturEvent(this, Type.MAX, this.getMaxItem());
+		this.extremValueChanged(pcEvent);
+	}
+
+
+	private void newMin() {
+		final TemperaturEvent pcEvent = new TemperaturEvent(this, Type.MIN, this.getMinItem());
+		this.extremValueChanged(pcEvent);		
+	} 
+
+	public void addPropertyChangeListener(MaxMinListener listener) {
+		if(listener != null) {        	
+			changeListeners.add(listener);
 		}
 	}
+
+	public void removePropertyChangeListener(MaxMinListener listener) {
+		if(listener != null) {        	
+			changeListeners.remove(listener);
+		}
+	}
+
+	@Override
+	public void extremValueChanged(TemperaturEvent pcEvent) {
+		for(final MaxMinListener listener : this.changeListeners) {
+			(listener).extremValueChanged(pcEvent);
+		}		
+	}
+
+
+	public String toString() {
+		return "Size: " + this.getCount() + "\nAverage: " + this.getAverage() + "\nMax: " + this.getMaxItem() + "\nMin: " + this.getMinItem();
+	}
+
+
 	
-	
-	
+
+
+
+
+
+
+
+
+
+
 }
