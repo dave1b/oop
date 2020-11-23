3987eab2423cc0333e894e02cd135e037ac49416
dave1b <57140925+dave1b@users.noreply.github.com>
2020-11-23 20:15:45 +0100

    Created an own EventListener Interface.

---
+++ src/main/java/ch/hslu/sw10/MaxMinListener.java
@@ -0,0 +1,15 @@
+package ch.hslu.sw10;
+
+
+
+import java.util.EventListener;
+
+public interface MaxMinListener extends EventListener {
+
+	public  void extremValueChanged(TemperaturEvent pcEvent);
+
+	
+	
+	
+	
+}
